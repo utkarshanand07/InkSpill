428e0242eb699c0d0e6f1641cd8a5cc1b51cb713
UtkarshAnand07 <utkarshanand7193@gmail.com>
2025-02-28 22:41:47 +0530

    path protected

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,8 +1,14 @@
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components'
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>Dashboard
+    <Button>
+      <LogoutLink>Logout</LogoutLink>
+    </Button>
+    </div>
   )
 }
 
